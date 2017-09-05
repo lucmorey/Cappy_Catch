997291c28d420f51e825d218abc80e6b75eac545
Luc Morey <lucmorey@gmail.com>
2017-09-05 10:54:08 -0700

    beginning js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 console.log('here')
 $
-var holes = $('.hole')
-var capybaraIMG = $("<img src= 'capybara.jpg' alt=''>");
 
+var capybaraIMG = $("<img src= 'capybara.jpg' alt=''>")
+var quokkaIMG = $("<img src= 'quokka.png' alt=''>")
+var squares = document.querySelectorAll('.square')
 
+function randomInt (n) {
+        return Math.floor(math.random()*n)
+    }
+function checkBox () {
+    if (this.innerHTML === capybaraIMG )
+        console.log('CAPPY')
+    if (this.innerHTML === quokkaIMG)
+        console.log ("QUOKKA")
+}
 
+squares[0].addEventListener('click', checkBox)
--- readme.txt
+++ readme.txt
@@ -1,4 +1,13 @@
 Cappy Catch
 
-Your yard has been overrun with capybaras! (A Brazilian marmot)
+Your yard has been overrun with Capybaras! (The largest rodent on earth!)
+Catch as many as you can, but don’t disturb the Quokkas! (Silly marsupials), AKA the happiest animals on earth.
+
+	Each player has one 30 second turn.
+	Click on as many Capybaras as you can.
+	Avoid the Quokkas!
+	+ 75 points for each capybara
+	-250 points for each Quokka
+
+ Player with most points in turn wins!
 

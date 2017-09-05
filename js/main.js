d5d0f8d31c4ce36ce91d468a4417c42d2c082567
Luc Morey <lucmorey@gmail.com>
2017-09-05 12:00:00 -0700

    beginning js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@ $
 
 var capybaraIMG = $("<img src= 'capybara.jpg' alt=''>")
 var quokkaIMG = $("<img src= 'quokka.png' alt=''>")
-var squares = document.querySelectorAll('.square')
+var $squares = $('.square')
+var $capybaras = $('img.capybara')
+var $quokkas = $('img.quokka')
+var $allImgs = $('img.piece')
+var randomMoveInterval = null
 
 function randomInt (n) {
-        return Math.floor(math.random()*n)
+        return Math.floor(Math.random()*n)
+}
+function $randomUnpopulatedSquare() {
+    var $randomSquare = $squares.eq(randomInt($squares.length))
+    if($randomSquare.html()){
+        return $randomUnpopulatedSquare()
+    } else {
+        return $randomSquare
     }
-function checkBox () {
-    if (this.innerHTML === capybaraIMG )
-        console.log('CAPPY')
-    if (this.innerHTML === quokkaIMG)
-        console.log ("QUOKKA")
 }
 
-squares[0].addEventListener('click', checkBox)
+$allImgs.on('click', function() {
+    $randomUnpopulatedSquare().append($(this))
+})
+$('#startBtn').on('click', function() {
+ randomMoveInterval = setInterval(function(){
+    $allImgs.each(function(index, $el) {
+        $randomUnpopulatedSquare().append($el)
+    })
+}, 1500)
+})
